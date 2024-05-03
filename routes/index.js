02ba286535139ad6813f15a4d468a26a757f4627
Julio Martin Sander <msander1987@gmail.com>
2024-05-03 15:58:31 -0300

    little changes in index route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-router.get('/', (req, res) => {res.send('Hello Martin');});
+router.get('/', (req, res) => {res.send('Welcome to project 1');});
 router.use('/users', require('./contacts'));
 
 module.exports = router;
