7a90bb1ec3d8ec970c046f976a7991828a5bbf2c
Julio Martin Sander <msander1987@gmail.com>
2024-05-03 13:45:20 -0300

    express

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-//Martin Sander
+const express = require('express');
+const app  = express();
+
+const port = process.env.PORT || 3000;
+
+//app.use('/', require('./routes'));
+
+app.listen(port, () => {console.log(`Running on port ${port}`)});
